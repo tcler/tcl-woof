006df73690bc806a238874c3ddb17a24d7c44355
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-01-03 16:50:29 +0530

    Generate ref docs in distribution and point to them from mgmt page

--- app/controllers/_woof/manage_controller.tcl
+++ app/controllers/_woof/manage_controller.tcl
@@ -20,9 +20,6 @@ oo::class create ManageController {
             stylesheets/_yui-2-8-0r4-base-min.css
             _woof.css
         }
-        set woof_url(user_guide) "http://woof.magicsplat.com/woof_guide"
-        set woof_url(quick_start) "http://woof.magicsplat.com/woof_guide/quick_start"
-        set woof_url(reference)   "http://woof.magicsplat.com/manuals/woof/index.html"
 
         pagevar set module_subheading "Site management"
     }
