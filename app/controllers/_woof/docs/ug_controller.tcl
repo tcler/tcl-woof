277c8d0aae868507b89416ccf27b015ef350c9f1
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2011-12-18 22:01:11 +0530

    Started IIS7 documentation

--- app/controllers/_woof/docs/ug_controller.tcl
+++ app/controllers/_woof/docs/ug_controller.tcl
@@ -36,7 +36,7 @@ oo::class create UgController {
             {apache_scgi "SCGI on Apache" 2}
             {apache_websh "Running as an Apache module" 2}
             {install_under_iis "Woof! under Microsoft IIS" 1}
-            {iis_scgi "SCGI on IIS" 2}
+            {iis_scgi "SCGI on IIS 5.1 and 6" 2}
             {install_under_lighttpd "Woof! under Lighttpd" 1}
             {lighttpd_scgi "SCGI on Lighttpd" 2}
             {start_scgi "Running as an SCGI server" 1}
